7d6f6205d68b340e21acddbc4898a7ba6c676645
Ruslan <ruslan.andrenov2003@gmail.com>
2023-01-29 20:09:48 +0800

    add gravity

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log('0');
+const cvs = document.querySelector('#canvas');
+const c = cvs.getContext('2d');
+
+cvs.width = 1200;
+cvs.height = 576;
+
+const gravity = 0.5;
+class Player {
+	constructor(position) {
+		this.position = position;
+		this.velocity = {
+			x: 0,
+			y: 1
+		};
+		this.height = 100;
+		this.width = 100;
+	}
+
+	draw() {
+		c.fillStyle = 'red';
+		c.fillRect(this.position.x, this.position.y, this.height, this.height);
+	}
+
+	update() {
+		this.draw();
+		this.position.y += this.velocity.y;
+		if (this.position.y + this.height + this.velocity.y < cvs.height) {
+			this.velocity.y += gravity;
+		} else {
+			this.velocity.y = 0;
+		}
+	}
+}
+
+const player = new Player({
+	x: 0,
+	y: 0
+});
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	c.fillStyle = 'white';
+	c.fillRect(0, 0, cvs.width, cvs.height);
+
+	player.update();
+}
+
+animate();
